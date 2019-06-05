0bef85e283ab82991800769c76ab2885d0db3803
agniveshreddy <m.agnivesh.reddy@gmail.com>
2019-06-05 15:48:22 +0200

    Update Section.js

--- DynamicTabBar/javascript/Section.js
+++ DynamicTabBar/javascript/Section.js
@@ -2,22 +2,22 @@ const _sectionElement = new WeakMap();
 
 class Section {
     constructor(sectionId) {
-		// this.sectionElement = document.getElementById(sectionId);
-		
-		_sectionElement.set(this, document.getElementById(sectionId));
+        // this.sectionElement = document.getElementById(sectionId);
+
+        _sectionElement.set(this, document.getElementById(sectionId));
     }
 
     show() {
-		// @todo Add implementation
-		
-		const sectionElement = _sectionElement.get(this);
+        // @todo Add implementation
+
+        const sectionElement = _sectionElement.get(this);
         sectionElement.classList.add('show');
     }
 
     hide() {
-		// @todo Add implementation
-		
-		const sectionElement = _sectionElement.get(this);
+        // @todo Add implementation
+
+        const sectionElement = _sectionElement.get(this);
         sectionElement.classList.remove('show');
     }
 }
